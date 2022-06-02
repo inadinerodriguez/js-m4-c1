6424ccf94aafb6626b8deee1e39d8b74c5e93f04
Nadine Rodriguez <inadinerod@gmail.com>
2022-06-01 20:27:20 -0400

    Inital commit

---
+++ js/script.js
@@ -0,0 +1,11 @@
+// console.log("hi");
+
+var mainTitle = document.querySelector("h1");
+console.log(mainTitle);
+
+mainTitle.innerText = "Yay, JavaScript!!";
+
+var body = document.querySelector("body");
+body.style.backgroundColor = "#FEF2F1";
+
+console.log(body);
